3236094eb72581a907ac54d1ab7ec27d0ccee105
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-11-15 15:13:45 -0400

    d

---
+++ RPG Game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Map.txt";
+            string input;
+            Console.WriteLine();
+            input = File.ReadAllText(path);
+            Console.WriteLine(path);
+        }
+
+    }
+
+}
